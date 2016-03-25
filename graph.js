aa1e00a6e73f809211f0fcc529f6e6958af020a7
Jiří Procházka <ojirio@gmail.com>
2016-03-25 15:46:19 +0100

    graphNode class

--- graph.js
+++ graph.js
@@ -11,13 +11,31 @@ class Graph {
         return {s: s, p: p, o: o};
     }
 
-    replaceNode(triple, position, replacement) {
-        var node = triple[position];
+    replaceNode(graphNode, replacement) {
+        var node = graphNode.getValue();
         this._graph = this._graph.map(function (triple2) {
             return { s: (triple2.s == node) ? replacement : triple2.s,
                 p: (triple2.p == node) ? replacement : triple2.p,
                 o: (triple2.o == node) ? replacement : triple2.o };
         });
-        triple[position] = replacement;
+        graphNode.setValue(replacement);
+    }
+}
+
+class GraphNode {
+    constructor(triple, position) {
+        this.triple = triple;
+        this.position = position;
+    }
+    
+    getValue() {
+        if (!this.triple || !this.position) {
+            return null;
+        }
+        return this.triple[this.position];
+    }
+    
+    setValue(value) {
+        this.triple[this.position] = value;
     }
 }
