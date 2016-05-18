11f650852931b811aff97f66597e9a97266906f3
Jiří Procházka <ojirio@gmail.com>
2016-05-18 23:38:17 +0200

    In build concatenate files according to the tsc build order (tsconfig.json file array)

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #/bin/sh
 rm output/*
 tsc -p .
-cat output/* > semaea.js
+rm semaea.js
+# concatenate output files, ordered by modification time to have class definitions 
+# ordered according to files array in tsconfig.json, to one output file 
+for i in `ls -tr output/`; do cat "output/$i" >> semaea.js; done
