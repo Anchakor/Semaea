6847c6a54f9e789a384d7b4f2a75db78e52709c6
Jiří Procházka <ojirio@gmail.com>
2016-05-21 17:32:13 +0200

    form abstraction

--- src/modals.ts
+++ src/modals.ts
@@ -1,26 +1,45 @@
-namespace Modals {  
-  export function formGetString(model: Model) {
+namespace Modals {
+  
+  type CloseFormFunction<T> = (form: any, isResolved: boolean, resolveRejectValue: T) => void
+  type FormFunction<T> = (closeForm: CloseFormFunction<T>, elementIdToBeFocused: string) => any
+  
+  function makeForm<T>(model: Model, formFunction: FormFunction<T>) {
     const pastFocus = document.activeElement;
-    const p = new Promise<string>((resolve, reject) => {
-      const position = model.modals.length;
+    const p = new Promise<T>((resolve, reject) => {
+      const modalPosition = model.modals.length;
+      const closeForm = (formToClose: any, isResolved: boolean, resolveRejectValue: T) => {
+        ModalsView.closeModal(model, formToClose);
+        (<HTMLElement>pastFocus).focus();
+        if (isResolved) {
+          resolve(resolveRejectValue);
+        } else {
+          reject(resolveRejectValue);
+        }
+      }
+      const form1 = formFunction(closeForm, 'modal'+modalPosition);
+      model.modals.push(form1);
+      model.elemIdToFocus = 'modal'+modalPosition;
+    }); 
+    //plastiq.html.refreshAfter(p);
+    return p;
+  }
+  
+  export function formGetString(model: Model) {
+    const formFunction = function (closeForm: CloseFormFunction<string>, elementIdToBeFocused: string) {
       const form = h('div', 
         h('input', {
-          type: 'text', id: 'modal'+position,
+          type: 'text', id: elementIdToBeFocused,
           onkeydown: function (e: KeyboardEvent) {
             if (e.keyCode == 13 /*enter*/) {
-              ModalsView.closeModal(model, form);
-              (<HTMLElement>pastFocus).focus();
-              resolve((<HTMLInputElement>e.target).value)
+              closeForm(form, true, (<HTMLInputElement>e.target).value);
               return false;
             }
             return true;
           }
         })
       );
-      model.modals.push(form);
-      model.elemIdToFocus = 'modal'+position;
-    }); 
-    //plastiq.html.refreshAfter(p);
-    return p;
+      return form;
+    }
+    return makeForm(model, formFunction);
   }
 }
