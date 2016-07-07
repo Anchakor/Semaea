014e686606040d8e3a29ab27fdee671fa6c2a722
Jiří Procházka <ojirio@gmail.com>
2016-07-07 19:55:30 +0200

    move modals code to their directory; use tsc --outFile now it works

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #/bin/sh
-rm output/*
-tsc -p .
+#rm output/*
+#tsc -p .
 rm semaea.js
-# concatenate output files, ordered by modification time to have class definitions 
-# ordered according to files array in tsconfig.json, to one output file 
-for i in `ls -tr output/`; do cat "output/$i" >> semaea.js; done
+tsc -p . --outFile semaea.js
--- src/modals/autocomplete.ts
+++ src/modals/autocomplete.ts
@@ -1,53 +1,5 @@
 namespace Modals {
-  
-  interface ICloseFormFunction<T> {
-    (form: any, isResolved: boolean, resolveRejectValue: T): void
-  }
-  interface IFormFunction<T> {
-    (closeForm: ICloseFormFunction<T>, elementIdToBeFocused: string): IComponent
-  }
-  
-  function makeForm<T>(model: Model, formFunction: IFormFunction<T>) {
-    const pastFocus = document.activeElement;
-    const p = new Promise<T>((resolve, reject) => {
-      const modalPosition = model.modals.length;
-      const closeForm: ICloseFormFunction<T> = (formToClose: any, isResolved: boolean, resolveRejectValue: T) => {
-        ModalsView.closeModal(model, formToClose);
-        (<HTMLElement>pastFocus).focus();
-        if (isResolved) {
-          resolve(resolveRejectValue);
-        } else {
-          reject(resolveRejectValue);
-        }
-      }
-      const form1 = formFunction(closeForm, 'modal'+modalPosition);
-      model.modals.push(form1);
-      model.elemIdToFocus = 'modal'+modalPosition;
-    }); 
-    //plastiq.html.refreshAfter(p);
-    return p;
-  }
-  
-  export function formGetString(model: Model) {
-    const formFunction = function (closeForm: ICloseFormFunction<string>, elementIdToBeFocused: string) {
-      const form = { 
-        render: (formArg) => { return h('div', 
-          h('input', {
-            type: 'text', id: elementIdToBeFocused,
-            onkeydown: function (e: KeyboardEvent) {
-              if (Key.isEnter(e)) {
-                closeForm(formArg, true, (<HTMLInputElement>e.target).value);
-                return false;
-              }
-              return true;
-            }
-          })
-        )}};
-      return form;
-    }
-    return makeForm(model, formFunction);
-  }
-  
+
   export namespace Autocomplete {
     class Form implements IComponent {
       render: (form: Form) => Plastiq.VNode
