e5a3541d56b264d024599f5a021d31460b016f48
Jiří Procházka <ojirio@gmail.com>
2016-11-28 02:05:39 +0100

    Attempt to make client side work again (failed).

--- src/Main.ts
+++ src/Main.ts
@@ -5,18 +5,20 @@ import { Graph } from "Graphs/Graph";
 import { Model, ModelMeta } from "Model";
 import * as GraphTests from "Graphs/GraphTests";
 
-const graph = new Graph();
-graph.addTriple(new Triple("testS", "testP", "testO"));
-graph.addTriple(new Triple("testS", "testP2", "testO"));
-graph.addTriple(new Triple("testO", "testP3", "testO3"));
+export function run() {
+  const graph = new Graph();
+  graph.addTriple(new Triple("testS", "testP", "testO"));
+  graph.addTriple(new Triple("testS", "testP2", "testO"));
+  graph.addTriple(new Triple("testO", "testP3", "testO3"));
 
-const testModelMeta = new ModelMeta();
-const model1 = new Model(graph);
+  const testModelMeta = new ModelMeta();
+  const model1 = new Model(graph);
 
-window.onload = function () {
-  plastiq.append($('#plastiq'), (model: Model) => { return MainView.render(model); }, model1);
-  
-  setInterval(() => { $('#graph').textContent = JSON.stringify(model1); }, 1000);
+  window.onload = function () {
+    plastiq.append($('#plastiq'), (model: Model) => { return MainView.render(model); }, model1);
+    
+    setInterval(() => { $('#graph').textContent = JSON.stringify(model1); }, 1000);
 
-  GraphTests.run();
+    GraphTests.run();
+  }
 }
