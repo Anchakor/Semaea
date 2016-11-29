85a77ed7cdfc072a96bd28b5d5bb3effd45711ba
Jiří Procházka <ojirio@gmail.com>
2016-11-29 22:28:27 +0100

    reference web global variables in a better way

--- src/External.ts
+++ src/External.ts
@@ -2,9 +2,9 @@ import { Plastiq, HtmlModule } from "plastiq";
 import { QUnitStatic } from "qunit";
 //import "../libraries/plastiq.js"
 
-export const $: any = eval("$");
-export const testlib: QUnitStatic = eval("testlib");
-export const plastiq: Plastiq = eval("plastiq");
+export const $: any = (<any>window).$;
+export const testlib: QUnitStatic = (<any>window).testlib;
+export const plastiq: Plastiq = (<any>window).plastiq;
 
-export const h: HtmlModule = eval("plastiq.html");
+export const h: HtmlModule = (<any>window).plastiq.html;
 //export const bind = plastiq.bind;
