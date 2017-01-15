88f0d51c1972de5fd69ec830e3c7453b7955d87c
Jiří Procházka <ojirio@gmail.com>
2017-01-15 17:24:18 +0100

    Add rootDir to make output dir functionality consistent (when having sources in multiple dirs)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 #rm -r output/*
 tsc -p . && \
-node node_modules/requirejs/bin/r.js -o baseUrl=output name=Main out=output/semaea_client.js optimize=none
+node node_modules/requirejs/bin/r.js -o baseUrl=output/src name=Main out=output/semaea_client.js optimize=none
