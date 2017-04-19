a9a51ad471a4f200eca85b6b8f089a5480e0b40e
Jiří Procházka <ojirio@gmail.com>
2017-04-19 09:10:46 +0200

    run integration test in parallel

--- src/Server/IntegrationTests.ts
+++ src/Server/IntegrationTests.ts
@@ -9,23 +9,23 @@ export function run() {
   Test.testAsync("Integration Filesystem ListDirectoryRequest", function(assert, asyncDone) {
     const c = new Request.ListDirectoryRequest();
     { c.dirPath = "test"; }
-    ServerClient.request(c, "ListDirectoryResponse")
+    const p1 = ServerClient.request(c, "ListDirectoryResponse")
     .then((response) => {
       const expected = new Response.ListDirectoryResponse();
       expected.listing = new Array<IDirectoryEntry>();
       expected.listing.push({ "kind": "directory", "name": "tdir" } as IDirectoryEntry);
       expected.listing.push({ "kind": "file", "name": "tfile.txt" } as IDirectoryEntry);
       assert.serializedEqual(response, expected, "When sending correct request, received correct ListDirectoryResponse");
-    })
-    .then(() => {
-      c.dirPath = "nonexistantTest";
-      return ServerClient.request(c, "ListDirectoryResponse")
-    })
+    });
+
+    c.dirPath = "nonexistantTest";
+    const p2 = ServerClient.request(c, "ListDirectoryResponse")
     .catch((response) => {
       const expected = new Response.ErrorResponse();
       expected.message = "{\"errno\":-2,\"code\":\"ENOENT\",\"syscall\":\"scandir\",\"path\":\"nonexistantTest\"}";
       assert.serializedEqual(response, expected, "When sending request for nonexisting directory, received correct ErrorResponse");
-      asyncDone();
     });
+
+    Promise.all([ p1, p2 ]).then(asyncDone).catch(asyncDone);
   });
 }
