450002fe536d82ec491713080cc7df932615d275
Jiří Procházka <ojirio@gmail.com>
2017-05-02 00:26:42 +0200

    fix External exports

--- src/External.ts
+++ src/External.ts
@@ -1,13 +1,14 @@
 import { QUnitStatic } from '../typings/qunit';
 import * as Inferno from 'inferno';
 import { default as InfernoHyperscript } from 'inferno-hyperscript';
-import { default as InfernoComponent,  } from 'inferno-component';
+import { default as InfernoComponent } from 'inferno-component';
 
 export const $: any = (<any>window).$;
 export const testlib: QUnitStatic = (<any>window).testlib;
-export const UILib = Inferno;
+
+export { Inferno as UILib };
 export const Component = InfernoComponent;
 export type VNode = Inferno.VNode;
 
 export const h = InfernoHyperscript;
-export const linkEvent = UILib.linkEvent;
+export const linkEvent = Inferno.linkEvent;
