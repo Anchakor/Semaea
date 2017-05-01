9544be04956d89c89c1b4dc86d23a3e4df042915
Jiří Procházka <ojirio@gmail.com>
2017-05-01 22:41:22 +0200

    TestingView of Inferno Component with state

--- src/Views/MainView.ts
+++ src/Views/MainView.ts
@@ -1,4 +1,4 @@
-import { h, $ } from '../External';
+import { h, $, Component } from '../External';
 import * as GraphView from '../Views/GraphView';
 import * as ModalsView from '../Views/ModalsView';
 import { Model } from '../Model';
@@ -9,7 +9,8 @@ export class MainView {
     
     return h('div', {}, [
       GraphView.render(model),
-      ModalsView.render(model)
+      ModalsView.render(model),
+      h(TestingView)
       ]);
   }
 
@@ -21,4 +22,18 @@ export class MainView {
       }
     }, 0);
   }
-} 
+}
+
+export class TestingView extends Component<{}, { x: number }> {
+  state = { x: 10 }
+  constructor(props?: {}, context?: any) { super(props, context); }
+  public render() {
+    setTimeout(() => {
+      this.setState({ x: this.state.x+1 });
+    }, 1000);
+    return h('p', {}, [
+      h('span', {}, 'counter: '),
+      h('span', {}, this.state.x.toString())
+    ]);
+  }
+}
