6046914382a7350f22c38ccc1d60b3babd639091
Jiří Procházka <ojirio@gmail.com>
2017-05-07 22:13:20 +0200

    clean up

--- build.js
+++ build.js
@@ -6,10 +6,7 @@ const path = require('path');
 const echo = console.log;
 const args = process.argv.slice(1);
 
-// modify this to control what gets built
-const modulesToBuild = [ 'build-Main.json'/*, 'build-Example1.json', 'build-Example2.json',*/];
-
-function mainRun(moduleConfig) {
+function mainRun() {
   switch (args[1]) {
     case 'debug':
       mainBuild() &&
@@ -24,8 +21,9 @@ function mainRun(moduleConfig) {
       echo("Simple build successful");
       break;
     case 'clean':
-      rmDir(moduleConfig.outputDir, false);
-      echo(`Output directory '${moduleConfig.outputDir}' was wiped`);
+      const outputDir = 'output';
+      rmDir(outputDir, false);
+      echo(`Output directory '${outputDir}' was wiped`);
       break;
     default:
       echo('To build use `node build.js debug` (or "release", "simple" or "clean").');
@@ -41,17 +39,6 @@ function sh(command, args) {
   if (p.stdout) { process.stdout.write(p.stdout); }
   if (p.stderr) { process.stderr.write(p.stderr); }
   return (p.status == 0)
-}
-
-function rJsPack(moduleConfig, noOptimize) {
-  var args = ['node_modules/requirejs/bin/r.js', '-o', 'rjsbuild.js',
-    'baseUrl='+moduleConfig.outputDir, 
-    'name='+moduleConfig.browserModule, 
-    'out='+moduleConfig.packOutputFilePath];
-  if (noOptimize) {
-    args.push("optimize=none")
-  }
-  return sh("node", args);
 }
 
 function rmDir(dirPath, removeSelf) {
@@ -71,28 +58,4 @@ function rmDir(dirPath, removeSelf) {
     fs.rmdirSync(dirPath);
 }
 
-class Config {
-  constructor() {
-    this.outputDir = 'output';
-    this.packOutputFileName = 'app_client.js';
-    this.browserModule = 'examples/Example1';
-    //this.packOutputFilePath = path.join(outputDir, packOutputFileName)
-  }
-}
-
-function getModuleConfig(moduleConfigFile) {
-  const fileContents = fs.readFileSync(moduleConfigFile);
-  var moduleConfig = JSON.parse(fileContents); // as Config
-  moduleConfig.packOutputFilePath = path.join(moduleConfig.outputDir, moduleConfig.packOutputFileName)
-  return moduleConfig;
-}
-
-function main() {
-  //sh("echo", ["arguments:"].concat(process.argv));
-  modulesToBuild.forEach(function(moduleConfigFile) {
-    const moduleConfig = getModuleConfig(moduleConfigFile);
-    mainRun(moduleConfig);
-  }, this);
-}
-
-main();
+mainRun();
