b0898f6caef8fdc4bdfd1405e528b177b5a88c07
Jiří Procházka <ojirio@gmail.com>
2017-07-02 13:02:55 +0200

    Some JSDoc comments

--- src/Common.ts
+++ src/Common.ts
@@ -8,7 +8,11 @@ export interface IComponent {
   render(): VNode
 }
 
-export const arrayImmutableSet = <T>(arr: Array<T>, ix: number, value: T): Array<T> => Object.assign([...arr], {[ix]: value});
+/** Create a copy of `array`, setting item at index `ix` to `value`. */
+export const arrayImmutableSet = <T>(array: Array<T>, ix: number, value: T): Array<T> => Object.assign([...array], {[ix]: value});
 
+/** Create a new object which is combinations of properies of `a` and `b` (`b` overriding `a`) */
 export const objectJoin = <A,B>(a: A, b: B): A & B => Object.assign({}, a, b);
+
+/** Create a new object as a colne of `a` */
 export const objectClone = <A>(a: A): A => Object.assign({}, a);
--- src/External.ts
+++ src/External.ts
@@ -17,6 +17,16 @@ export const linkEvent = Inferno.linkEvent;
 
 export { Redux as StoreLib };
 export { InfernoRedux as UIStoreLib };
+
+/** Connect UIComponent to UIStore.
+ * `component` has props made by 
+ * 1) `mapStateToProps` (usually use `objectJoin` on the entire `state` plus object of select relevant properties deeper in the state)
+ * 2) `mapDispatchToProps` (create props trigger functions actions using `dispatch`)
+ * @param component The components (extends UIComponent). 
+ *    Has props object of union of output of `mapStateToProps` and `mapDispatchToProps`.
+ * @param mapStateToProps Maps state into props for `component` (usually use `objectJoin` on the entire `state` plus object of select relevant properties deeper in the state).
+ * @param mapDispatchToProps Create props trigger functions actions using `dispatch` function.
+ */
 export const connect = <P1, P2, C extends InfernoComponent<P1 & P2, {}>, OwnProps>(
   component: new () => C, 
   mapStateToProps?: (state: any, ownProps?: OwnProps) => P1, 
