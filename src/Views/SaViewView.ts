0d1f14c67cf64d88eb27875f98f5a57cadf2538c
Jiří Procházka <ojirio@gmail.com>
2018-01-22 23:52:43 +0100

    Show only SaViews which are not linked to Dialogs  which should not be visible

--- src/Views/SaViewView.ts
+++ src/Views/SaViewView.ts
@@ -2,6 +2,7 @@ import { StoreState } from '../UIStore/Main';
 import { SaView, createChangeSaViewAction } from '../UIStore/SaViews';
 import { connect, h, StoreLib, UIComponent } from '../External';
 import { objectJoin } from '../Common';
+import { shouldDialogBeVisible } from 'Dialogs/Dialogs';
 
 // View (component):
 
@@ -23,18 +24,34 @@ export class View extends UIComponent<Props, {}> {
   }
 
   private renderSaViewSwitchingBar() {
+    const shouldBeVisibleBasedOnLinkedDialogs = (saViewIndex: number) => {
+      // if the SaView is not linked to Dialogs which should not be visible
+      const linkedDialogs = this.props.dialogs_.viewMappings.filter((v, ix, arr) => {
+        return v.saViewIndex == saViewIndex;
+      });
+      if (linkedDialogs.length == 0) return true;
+      return linkedDialogs.filter((v, ix, arr) => {
+        const dialog = this.props.dialogs_.dialogs[v.dialogIndex];
+        if (!dialog) return false;
+        return shouldDialogBeVisible(dialog);
+      }).length > 0;
+    };
     return h('div', {}, [
       h('span', {}, "Views: ")
-    ].concat(this.props.saViews.map((g, i) => {
-      let tagClass: string = '';
-      if (this.props.saViewIndex == i) {
-        tagClass = 'element-selected'
-      }
-      return h('button', { 
-        class: tagClass,
-        onclick: () => this.props.changeCurrentSaView(i)
-      }, i.toString())
-    })));
+      ].concat(this.props.saViews.map((saView, saViewIndex) => saViewIndex)
+        .filter((saViewIndex) => shouldBeVisibleBasedOnLinkedDialogs(saViewIndex))
+        .map((saViewIndex) => {
+          let tagClass: string = '';
+          if (this.props.saViewIndex == saViewIndex) {
+            tagClass = 'element-selected'
+          }
+          return h('button', { 
+            class: tagClass,
+            onclick: () => this.props.changeCurrentSaView(saViewIndex)
+          }, saViewIndex.toString())
+        })
+      )
+    );
   }
 }
 
