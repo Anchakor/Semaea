8313de44ac32dfe6307675251bc5c7ed2744ab4d
Jiří Procházka <ojirio@gmail.com>
2018-02-13 00:19:47 +0100

    Fix refocusing from dialog forms etc. Still not ideal, as it does refocus to graph nodes when changing SaViews etc.

--- src/Views/EntityView.ts
+++ src/Views/EntityView.ts
@@ -20,7 +20,7 @@ function isSomeOccurenceOfCurrentGraphNode(props: Props): boolean {
   return props.saGraphView.currentNode.getValue() == props.graphNode.getValue();
 }
 
-export class EntityView extends UIComponent<Props, { elem: HTMLElement }> {
+export class EntityView extends UIComponent<Props, { elem: HTMLElement, wasFocused: boolean }> {
   constructor(props?: Props, context?: any) { super(props, context); }
   render() {
     let innerProps = objectJoinExtend(this.props, {
@@ -28,9 +28,12 @@ export class EntityView extends UIComponent<Props, { elem: HTMLElement }> {
         this.setState({ elem: e }); 
       },
       onComponentDidUpdate: (lastProps: Props, nextProps: Props) => { 
-        if (isCurrentGraphNode(nextProps) && this.state) 
-          // TODO fix refocusing from dialog forms etc.
+        if (isCurrentGraphNode(nextProps) && this.state && !this.state.wasFocused) {
           this.state.elem.focus();
+          this.setState({ wasFocused: true });
+        } else if (this.state && this.state.wasFocused && !isCurrentGraphNode(nextProps)) {
+          this.setState({ wasFocused: false });
+        }
       }
     });
     return h(EntityViewInner, innerProps);
