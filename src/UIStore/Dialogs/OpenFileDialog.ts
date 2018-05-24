25ed2133f8fbccf39960b84693a132b00f20bf41
Jiří Procházka <ojirio@gmail.com>
2018-05-24 21:47:10 +0200

    Common doAction method for OpenFileDialog reducers

--- src/UIStore/Dialogs/OpenFileDialog.ts
+++ src/UIStore/Dialogs/OpenFileDialog.ts
@@ -1,17 +1,18 @@
 import { StoreLib } from '../../External';
 import { StoreState } from '../Main';
-import { OpenFileDialog, Status as DialogStatus, DialogKind, dialogIsOfKind } from '../../Dialogs/Dialog';
+import { OpenFileDialog, Status as DialogStatus, DialogKind, dialogIsOfKind, DialogSaViewMapping } from '../../Dialogs/Dialog';
 import { doCreateDialog } from '../Dialogs';
 import { GraphNode } from '../../Graphs/GraphNode';
 import { Triple } from '../../Graphs/Triple';
 import { Graph } from '../../Graphs/Graph';
-import { objectJoin, arrayImmutableAppend, Log, arrayImmutableSet, filterDownArray, filterDownArrayToIndexed } from '../../Common';
+import { objectJoin, arrayImmutableAppend, Log, arrayImmutableSet, filterDownArray, filterDownArrayToIndexed, getIndexedArray } from '../../Common';
 import { request } from '../../Server/Client';
 import { ListDirectoryResponse, ResponseKind, responseIsOfKind, handleUnexpectedResponse } from '../../Server/Response';
 import { ListDirectoryRequest } from '../../Server/Request';
 import { normalize } from 'path';
 import { createDefaultGraphFilter } from 'UIStore/GraphFilters';
 import { DirectoryEntryKind, FilesystemPredicates } from '../../Entities/Filesystem';
+import { SaGraphView } from 'UIStore/Graphs';
 
 export const createOpenFileDialog = (directoryPath: string, originatingSaViewIndex: number) => (dispatch: (a: StoreLib.Action) => void) => {
   directoryPath = normalize(directoryPath);
@@ -97,7 +98,7 @@ export enum ActionType { OpenFileDialogChangeDirectory = 'OpenFileDialogChangeDi
 export interface OpenFileDialogChangeDirectoryAction extends StoreLib.Action { type: ActionType.OpenFileDialogChangeDirectory
   dialogIndex: number
   directoryPath: string
-  syncID: number
+  syncID: number // this will be written, not used for searching
 }
 function createOpenFileDialogChangeDirectoryAction(partialAction: Partial<OpenFileDialogChangeDirectoryAction>) { 
   return objectJoin(objectJoin<OpenFileDialogChangeDirectoryAction>({ type: ActionType.OpenFileDialogChangeDirectory,
@@ -107,37 +108,18 @@ function createOpenFileDialogChangeDirectoryAction(partialAction: Partial<OpenFi
   }, partialAction), { syncID: SyncID.getNext() });
 }
 function doOpenFileDialogChangeDirectoryAction(state: StoreState, action: OpenFileDialogChangeDirectoryAction) {
-  const dialogIndexed = filterDownArrayToIndexed(state.dialogs_.dialogs, dialogIsOfKind(DialogKind.OpenFile))
-    .find((v) => v.index == action.dialogIndex);
-  if (!dialogIndexed || !dialogIndexed.value) {
-    Log.error("Dialog not found: "+JSON.stringify(action));
-    return state;
+  function getNewDialog(action: OpenFileDialogChangeDirectoryAction, dialog: OpenFileDialog): OpenFileDialog | undefined {
+    return objectJoin(dialog, 
+      { listDirectoryStatus: 'loading', directoryPath: action.directoryPath, syncID: action.syncID });
   }
-  const dialog = dialogIndexed.value;
-  const newDialog = objectJoin(dialog, 
-    { listDirectoryStatus: 'loading', directoryPath: action.directoryPath, syncID: action.syncID });
-  
-  // Reseting dialog filter
-  const mapping = state.dialogs_.viewMappings.find((v) => v.dialogIndex == action.dialogIndex);
-  if (!mapping) {
-    Log.error("Dialog mapping not found: "+JSON.stringify(action));
-    return state; 
+  function getNewGraph(action: OpenFileDialogChangeDirectoryAction, graph: Graph): Graph | undefined {
+    return new Graph();
   }
-  const saGraphViewIndex = state.saViews_.saViews[mapping.saViewIndex].saGraphViewIndex;
-  const saGraphView = state.graphs_.saGraphViews[saGraphViewIndex];
-  const saGraphViews = arrayImmutableSet(state.graphs_.saGraphViews, saGraphViewIndex, 
-    objectJoin(saGraphView, { filter: createGraphFilter() }));
-  const graphs = arrayImmutableSet(state.graphs_.graphs, saGraphView.graphIndex, new Graph());
-
-  const newState = objectJoin<StoreState>(state, { 
-    dialogs_: objectJoin(state.dialogs_, { 
-      dialogs: arrayImmutableSet(state.dialogs_.dialogs, dialogIndexed.index, newDialog)
-    }),
-    graphs_: objectJoin(state.graphs_, { saGraphViews: saGraphViews,
-      graphs: graphs
-    }),
-  });
-  return newState;
+  function getNewSaGraphView(action: OpenFileDialogChangeDirectoryAction, saGraphView: SaGraphView): SaGraphView | undefined {
+    // Reseting dialog filter
+    return objectJoin(saGraphView, { filter: createGraphFilter() });
+  }
+  return doAction(state, action, getNewDialog, getNewGraph, getNewSaGraphView);
 }
 
 // AddOpenFileDialogDirectoryListingAction
@@ -156,27 +138,80 @@ function createAddOpenFileDialogDirectoryListingAction(partialAction: Partial<Ad
   }, partialAction);
 }
 function doAddOpenFileDialogDirectoryListingAction(state: StoreState, action: AddOpenFileDialogDirectoryListingAction) {
-  const dialogIndexed = filterDownArrayToIndexed(state.dialogs_.dialogs, dialogIsOfKind(DialogKind.OpenFile))
-    .find((v) => v.value.syncID == action.syncID);
-  if (!dialogIndexed || !dialogIndexed.value) return state;
-  const dialog = dialogIndexed.value;
-  if (dialog.listDirectoryStatus != 'loading') { 
-    Log.error("Not in 'loading' status: "+JSON.stringify(dialog)) 
+  function getNewDialog(action: AddOpenFileDialogDirectoryListingAction, dialog: OpenFileDialog): OpenFileDialog | undefined {
+    return objectJoin(dialog, { listDirectoryStatus: 'loaded' });
   }
+  function getNewGraph(action: AddOpenFileDialogDirectoryListingAction, graph: Graph): Graph | undefined {
+    return action.graph.clone();
+  }
+  return doAction(state, action, getNewDialog, getNewGraph, undefined);
+}
+
+// Common: 
+
+function doAction<A>(state: StoreState, 
+  action: A & { dialogIndex?: number, syncID: number },
+  getNewDialog?: (action: A, dialog: OpenFileDialog) => OpenFileDialog | undefined,
+  getNewGraph?: (action: A, graph: Graph) => Graph | undefined,
+  getNewSaGraphView?: (action: A, saGraphView: SaGraphView) => SaGraphView | undefined,
+) {
+  const dialogIndexed = filterDownArrayToIndexed(state.dialogs_.dialogs, dialogIsOfKind(DialogKind.OpenFile))
+    .find((v) => ((action.dialogIndex == undefined) 
+      ? v.value.syncID == action.syncID 
+      : v.index == action.dialogIndex) );
+  if (!dialogIndexed || !dialogIndexed.value) {
+    Log.error("Dialog not found: "+JSON.stringify(action));
+    return state;
+  }
+  const dialog = dialogIndexed.value;
+
   const graph = state.graphs_.graphs[dialog.createdGraphIndex];
   if (!graph) { 
     Log.error("createdGraphIndex is invalid: "+JSON.stringify(dialog)) 
     return state;
   }
-  const newGraph = action.graph.clone();
-  const newDialog = objectJoin(dialog, { listDirectoryStatus: 'loaded' });
+
+  const matchingSaGraphViewIndexes = getIndexedArray(state.graphs_.saGraphViews)
+    .filter((v) => v.value.graphIndex == dialog.createdGraphIndex)
+    .map((v) => v.index);
+  if (matchingSaGraphViewIndexes.length != 1) {
+    Log.error(`Found ${matchingSaGraphViewIndexes.length} (not 1) of SaGraphViews for open file dialog: ${JSON.stringify(action)}`);
+    return state; 
+  }
+  const saGraphViewIndex = matchingSaGraphViewIndexes[0];
+  const saGraphView = state.graphs_.saGraphViews[saGraphViewIndex];
+
+  // processing:
+
+  let newDialogs = state.dialogs_.dialogs;
+  if (getNewDialog) {
+    const newDialog = getNewDialog(action, dialog);
+    if (newDialog == undefined) return state;
+    newDialogs = arrayImmutableSet(state.dialogs_.dialogs, dialogIndexed.index, newDialog);
+  }
+
+  let newGraphs = state.graphs_.graphs;
+  if (getNewGraph) {
+    const newGraph = getNewGraph(action, graph);
+    if (newGraph == undefined) return state;
+    newGraphs = arrayImmutableSet(state.graphs_.graphs, dialog.createdGraphIndex, newGraph);
+  }
+
+  let newSaGraphViews = state.graphs_.saGraphViews;
+  if (getNewSaGraphView) {
+    const newSaGraphView = getNewSaGraphView(action, saGraphView);
+    if (newSaGraphView == undefined) return state;
+    newSaGraphViews = arrayImmutableSet(state.graphs_.saGraphViews, saGraphViewIndex, newSaGraphView);
+  }
+
   const newState = objectJoin<StoreState>(state, { 
     dialogs_: objectJoin(state.dialogs_, { 
-      dialogs: arrayImmutableSet(state.dialogs_.dialogs, dialogIndexed.index, newDialog)
+      dialogs: newDialogs
     }),
     graphs_: objectJoin(state.graphs_, { 
-      graphs: arrayImmutableSet(state.graphs_.graphs, dialog.createdGraphIndex, newGraph)
-    })
+      saGraphViews: newSaGraphViews,
+      graphs: newGraphs
+    }),
   });
   return newState;
 }
