a3bf55eda1515c43a6f3827de25af0178365a99a
Jiří Procházka <ojirio@gmail.com>
2020-04-19 18:25:38 +0200

    Optimize getCurrentProps with getters computing only requested item

--- src/Views/CurrentProps.ts
+++ src/Views/CurrentProps.ts
@@ -15,25 +15,20 @@ export interface CurrentProps {
 }
 
 export function getCurrentProps(state: StoreState): CurrentProps {
-  const saViewIndex = state.saViews_.currentSaViewIndex;
-  const saView = state.saViews_.saViews[saViewIndex];
-  const saGraphViewIndex = saView.saGraphViewIndex;
-  const saGraphView = state.graphs_.saGraphViews[saGraphViewIndex];
-  const graph = state.graphs_.graphs[saGraphView.graphIndex];
-  const dialogs = state.dialogs_.dialogs;
-  const dialogSaViewMappings = state.dialogs_.viewMappings;
-  const matchingMappings = dialogSaViewMappings.filter((mapping, i, arr) => mapping.saViewIndex == saViewIndex);
-  const dialogIndex = (matchingMappings.length > 0) ? matchingMappings[0].dialogIndex : undefined;
-  const dialog = (dialogIndex != undefined) ? state.dialogs_.dialogs[dialogIndex] : undefined;
-
   const currentProps: CurrentProps = {
-    saViewIndex: saViewIndex,
-    saView: saView,
-    saGraphViewIndex: saGraphViewIndex,
-    saGraphView: saGraphView,
-    graph: graph,
-    dialogIndex: dialogIndex,
-    dialog: dialog
+    get saViewIndex() { return state.saViews_.currentSaViewIndex; },
+    get saView() { return state.saViews_.saViews[this.saViewIndex]; },
+    get saGraphViewIndex() { return this.saView.saGraphViewIndex; },
+    get saGraphView() { return state.graphs_.saGraphViews[this.saGraphViewIndex]; },
+    get graph() { return state.graphs_.graphs[this.saGraphView.graphIndex]; },
+    get dialogIndex() {
+      const dialogSaViewMappings = state.dialogs_.viewMappings;
+      const matchingMappings = dialogSaViewMappings
+        .filter((mapping, i, arr) => mapping.saViewIndex == this.saViewIndex);
+      return (matchingMappings.length > 0) ? matchingMappings[0].dialogIndex : undefined;
+    },
+    get dialog() { return (this.dialogIndex != undefined) 
+      ? state.dialogs_.dialogs[this.dialogIndex] : undefined; },
   }
 
   return currentProps;
