a496c97ac2195e73bec369fc4e6ed3939cdc5a1f
Jiří Procházka <ojirio@gmail.com>
2020-04-26 14:27:01 +0200

    Minimizing props of FocusableComponent

--- src/Views/FocusableComponent.ts
+++ src/Views/FocusableComponent.ts
@@ -1,11 +1,10 @@
 import { objectJoinExtend, Log } from '../Common';
-import { StoreState } from '../UIStore/Main';
-import { MainDispatchProps } from './MainDispatchProps';
+import { DispatchProps } from '../UIStore/Main';
 import { UIComponent, FunctionalUIComponent, hf } from '../External';
-import { FocusTarget } from '../UIStore/Focus';
+import { FocusTarget, createSetChangeFocusToNoneAction, State as FocusState } from '../UIStore/Focus';
 
 /** Base class for focusable UI Components. */
-export abstract class FocusableComponent<TProps extends StoreState & MainDispatchProps> extends UIComponent<TProps, { elem: HTMLElement }> {
+export abstract class FocusableComponent<TProps extends DispatchProps & { focus_: FocusState}> extends UIComponent<TProps, { elem: HTMLElement }> {
   constructor(props: TProps, context?: any) { super(props, context); }
   render() {
     let innerProps = objectJoinExtend(this.props, {
@@ -13,12 +12,11 @@ export abstract class FocusableComponent<TProps extends StoreState & MainDispatc
         this.setState({ elem: e }); 
       },
       onComponentDidUpdate: (lastProps: TProps, nextProps: TProps) => { 
-        if (this.state && nextProps.focus_.changeFocusTo && this.additionalFocusCondition(lastProps, nextProps)
-          && nextProps.focus_.changeFocusTo == this.focusTarget) {
+        if (this.state && nextProps.focus_.changeFocusTo && nextProps.focus_.changeFocusTo == this.focusTarget) {
             Log.debug("Focusing FocusableComponent: "+this.getInnerComponentName());
             // Look for the debug logging between non-empty SetChangeFocusTo action and the empty one (acknowledge)
             this.state.elem.focus();
-            nextProps.acknowledgeFocusChange();
+            nextProps.dispatch(createSetChangeFocusToNoneAction());
         }
       }
     });
@@ -27,7 +25,6 @@ export abstract class FocusableComponent<TProps extends StoreState & MainDispatc
 
   abstract readonly innerComponent: FunctionalUIComponent<TProps>;
   abstract readonly focusTarget: FocusTarget;
-  additionalFocusCondition(lastProps: TProps, nextProps: TProps) { return true; };
   innerComponentName?: string = undefined;
   private getInnerComponentName() { return (this.innerComponentName) ? this.innerComponentName :  this.innerComponent.name; }
 }
