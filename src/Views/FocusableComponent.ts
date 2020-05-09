304f0c47e6bcc319a6cb66a6c1c103fb1c56c136
Jiří Procházka <ojirio@gmail.com>
2020-05-10 01:00:30 +0200

    Small refactor

--- src/Views/FocusableComponent.ts
+++ src/Views/FocusableComponent.ts
@@ -1,23 +1,23 @@
 import { objectJoinExtend, Log } from '../Common';
-import { DispatchProps } from '../UIStore/Main';
+import { DispatchProps, FocusProps } from '../UIStore/Main';
 import { UIComponent, FunctionalUIComponent, hf } from '../External';
 import { FocusTarget, createSetChangeFocusToNoneAction, State as FocusState } from '../UIStore/Focus';
 
-type FocusProps = DispatchProps & { focus_: FocusState };
+type Props = DispatchProps & FocusProps;
 
 /** Base class for focusable UI Components. */
 export function withFocusable<InnerProps>(
   innerComponent: FunctionalUIComponent<InnerProps>, 
   focusTarget: FocusTarget,
   innerComponentName?: (p: InnerProps) => string) {
-return class FocusableComponent extends UIComponent<InnerProps & FocusProps, { elem: HTMLElement }> {
-    constructor(props: InnerProps & FocusProps, context?: any) { super(props, context); }
+return class FocusableComponent extends UIComponent<InnerProps & Props, { elem: HTMLElement }> {
+    constructor(props: InnerProps & Props, context?: any) { super(props, context); }
     render() {
       let innerProps: InnerProps = objectJoinExtend(this.props, {
         onComponentDidMount: (e: HTMLElement) => { 
           this.setState({ elem: e }); 
         },
-        onComponentDidUpdate: (lastProps: InnerProps & FocusProps, nextProps: InnerProps & FocusProps) => { 
+        onComponentDidUpdate: (lastProps: InnerProps & Props, nextProps: InnerProps & Props) => { 
           if (this.state && nextProps.focus_.changeFocusTo && nextProps.focus_.changeFocusTo == focusTarget) {
               Log.debug("Focusing FocusableComponent: "+this.getInnerComponentName());
               // Look for the debug logging between non-empty SetChangeFocusTo action and the empty one (acknowledge)
